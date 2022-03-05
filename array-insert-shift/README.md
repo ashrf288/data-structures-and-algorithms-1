33cf7257040c30418a6e2d078f6ff549e2237d2f
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-03-05 15:15:20 +0300

    finish challenge 2 saturday

--- array-insert-shift/README.md
+++ array-insert-shift/README.md
@@ -1,8 +1,9 @@
 # Insert to Middle of an Array
-<!-- Description of the challenge -->
+ 1- Write a function called insertShiftArray which takes in an array and a value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
+ 
+ 2- Write a second function that removes an element from the middle index and shifts other elements in the array to fill the new gap.
 
 ## Whiteboard Process
-<!-- Embedded whiteboard image -->
+ 
 
-## Approach & Efficiency
-<!-- What approach did you take? Discuss Why. What is the Big O space/time for this approach? -->
+ 
--- array-insert-shift/tests/test_array_insert_shift.py
+++ array-insert-shift/tests/test_array_insert_shift.py
@@ -1,5 +1,7 @@
 from array_insert_shift import __version__
 from array_insert_shift.the_code import insertShiftArray
+from array_insert_shift.the_code import removeShiftArray
+
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -14,10 +16,23 @@ def test_odd_length():
     expected=[1,2,3,100,4,5]
     assert actual==expected
 
-def test_long_length():
+def test_long_array_length():
     actual=insertShiftArray([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],100)
 
     expected=[1,2,3,4,5,6,7,8,9,10,100,11,12,13,14,15,16,17,18,19,20]
     assert actual==expected
+
+
+########################## 
+def test_remove_odd_length():
+        actual=removeShiftArray([3,3,4,1,5,55,5])
+        expected=[3, 3, 4, 5, 55, 5]
+        assert actual==expected
+
+    
+def test_remove_even_length():
+        actual=removeShiftArray([1,2,3,4])
+        expected=[1, 2, 4]
+        assert actual==expected
 
 
